7cddd097adbac1f0944884fa0310e33dd7a54539
mpcahn <mpcahn@gmail.com>
2018-08-27 10:55:26 -0600

    now it works

--- roman.js
+++ roman.js
@@ -1,53 +1,33 @@
 var romanNum = {
-  'weird': {
-    'IV': 4,
-    'IX': 9,
-    'XL': 40,
-    'XC': 90,
-    'CD': 400,
-    'CM': 900
-  },
-  'normal': {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-  }
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000
 }
 
 function romanToDecimal(str) {
   var count = 0
   var i = 0
 
-  while (i < str.length) {
-    let sym = str.charAt(i)
-    let deuc = sym + str.charAt(i + 1)
+  for (var i = 0; i < str.length; i++) {
+    var sym = romanNum[str.charAt(i)]
+    var fut = romanNum[str.charAt(i+1)]
 
-    for (key in romanNum['weird']) {
-      if (key === deuc) {
-        count += romanNum['weird'][key]
-        i += 2
-        break;
-      }
-    }
-
-    for (key in romanNum['normal']) {
-      if (key === sym) {
-        count += romanNum['normal'][key]
-        i++
-        break;
-      }
+    if (fut > sym){
+      count -= sym 
+    } else {
+      count += sym
     }
   }
 
   return count
 }
 
-console.log(romanToDecimal('VII'))
-console.log(romanToDecimal('XXXXII'))
-console.log(romanToDecimal('CIV'))
-console.log(romanToDecimal('CIX'))
-console.log(romanToDecimal('MXC'))
+console.log(romanToDecimal('VII'), ': 7')
+console.log(romanToDecimal('XLII'), ': 42')
+console.log(romanToDecimal('CIV'), ': 104')
+console.log(romanToDecimal('CIX'), ': 109')
+console.log(romanToDecimal('MXC'), ': 1090')
